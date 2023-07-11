cab121be69800e65d786a141a48f849c5d2c38df
David Ogborn <ogbornd@mcmaster.ca>
2023-07-11 14:29:48 -0400

    after 11 July 2023

--- Player.gd
+++ Player.gd
@@ -1,6 +1,39 @@
 extends KinematicBody
 
+var velocity = Vector3();
+var fall = Vector3();
+var speed = 18;
+var acceleration = 6;
+var gravity = 1;
+var jump = 18;
+var mouse_sensitivity = 0.05;
+
 func _ready():
 	var camera = Camera.new();
 	camera.name ="Camera";
 	add_child(camera);
+	# TODO: need to add collision shape from code instead of in node graph
+	
+func _physics_process(delta):
+	if global_translation.y > 0:
+		move_and_slide(fall, Vector3.UP)
+	if not is_on_floor():
+		fall.y -= gravity
+	if Input.is_action_just_pressed("a_button") and is_on_floor():
+		fall.y = jump
+	var leftStick = Input.get_vector("left_stick_left","left_stick_right","left_stick_up","left_stick_down");
+	rotate_y(-leftStick.x*0.04); 
+	var camera = $Camera;
+	camera.rotation.x -= leftStick.y * 0.04;
+	camera.rotation.x = clamp(camera.rotation.x, deg2rad(-90), deg2rad(90));
+	# var wasd = Input.get_vector("right_stick_up","right_stick_down","right_stick_left","right_stick_right");
+	var xMove = Input.get_axis("right_stick_up","right_stick_down");
+	var yMove = Input.get_axis("right_stick_left","right_stick_right"); # not sure why is always 0
+	var direction = Vector3(0,0,0);
+	#direction += xMove * transform.basis.x;
+	direction += xMove * transform.basis.z;
+	if direction.length() > 1:
+		direction = direction.normalized();
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta);
+	velocity = move_and_slide(velocity, Vector3.UP);
+
--- World.gd
+++ World.gd
@@ -1,18 +1,37 @@
 extends Spatial
 
+var frame = 0;
+var rng = RandomNumberGenerator.new()
+var j = 0; #x position
+var k = 0; #y-position
+
 func _ready():
 	makeACube(0,0,-10);
-	makeACube(3,0,-10);
-	makeACube(-3,0,-10);
-			  
-func makeACube(x,y,z):
-	print("makeACube(" + str(x) + "," + str(y) + "," + str(z) + ")");
+	makeACube(3,0,-10,0,1,0);
+	makeACube(-3,0,-10,0,0,1);
+	#makeACube(3.5,1,-9,1,6,1);
+
+
+
+func _physics_process(_delta):
+	frame = frame + 1;
+	if frame%60 == 0:
+		#moved in scope to be used in _physics_process
+		var random_number_A = rng.randf_range(0.0, 1.0)
+		var random_number_B = rng.randf_range(0.0, 1.0)
+		var random_number_C = rng.randf_range(0.0, 1.0)
+		makeACube(6+3*j,0,-10,random_number_A,random_number_B, random_number_C);
+		makeACube(-(6+3*j),0,-10,random_number_A,random_number_B,random_number_C);
+		j = j+1;
+
+				
+
+func makeACube(x=0,y=0,z=0,r=1,g=0,b=0):
 	var m = MeshInstance.new();
 	var cm = CubeMesh.new();
 	var sm = SpatialMaterial.new();
-	sm.albedo_color = Color( 1, 0, 0, 1 );
+	sm.albedo_color = Color( r, g, b, 1 );
 	cm.material = sm;
 	m.mesh = cm;
 	m.translation = Vector3(x,y,z);
 	add_child(m);
-	
