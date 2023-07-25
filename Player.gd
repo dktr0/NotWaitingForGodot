b0e79d69d1a95b834179269ba9af12d9a687296f
David Ogborn <ogbornd@mcmaster.ca>
2023-07-25 13:37:32 -0400

    progress for July 25 2023

--- Player.gd
+++ Player.gd
@@ -8,26 +8,29 @@ var gravity = 1;
 var jump = 18;
 var mouse_sensitivity = 0.05;
 
+onready var camera = $"/root/NotWaitingForGodot/Player/Camera";
+
 func _ready():
+	print(str(camera));
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	var camera = Camera.new();
-	camera.name ="Camera";
-	add_child(camera);
+	#var camera = Camera.new();
+	#camera.name ="Camera";
+	#add_child(camera);
 	# TODO: need to add collision shape from code instead of in node graph
 
 func _input(event):	
 	if event is InputEventMouseMotion:
 		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity)) 
-		$Camera.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity)) 
-		$Camera.rotation.x = clamp($Camera.rotation.x, deg2rad(-90), deg2rad(90))
+		camera.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity)) 
+		camera.rotation.x = clamp(camera.rotation.x, deg2rad(-90), deg2rad(90))
 	if event is InputEventKey and event.scancode == KEY_ESCAPE:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
-	#if global_translation.y > -10:
-#		move_and_slide(fall, Vector3.UP)
+	if global_translation.y > -20:
+		move_and_slide(fall, Vector3.UP)
 	if not is_on_floor():
 		fall.y -= gravity
 	if Input.is_action_just_pressed("a_button") and is_on_floor():
@@ -35,7 +38,6 @@ func _physics_process(delta):
 	var leftStick = Input.get_vector("left_stick_left","left_stick_right","left_stick_up","left_stick_down");
 	var rightStick = Input.get_vector("right_stick_left","right_stick_right","right_stick_up","right_stick_down");
 	rotate_y(-rightStick.x*0.04); 
-	var camera = $Camera;
 	camera.rotation.x -= rightStick.y * 0.04;
 	camera.rotation.x = clamp(camera.rotation.x, deg2rad(-90), deg2rad(90));
 	var xMove = leftStick.x;
--- World.gd
+++ World.gd
@@ -6,14 +6,19 @@ var j = 0; #x position
 var k = 0; #y-position
 
 func _ready():
+	cubeMesh(0,-2,-10,50,50);
 	makeASphere(0,0,-10,0,0,1);
+	tree(0,0,0);
+	tree(10,0,0);
+	tree(20,0,0);
+	
 	agasthyaReady();
 	fatimaReady();
 	nadiaReady();
-	yakariReady();
-	jamesReady();
 	davidReady();
-	
+	cyrilReady();
+
+
 func davidReady():
 	cubeMesh(0,-2,-10,30,30);
 	cubeRow("++++++++++++++++++++",240,0,-10,2,0,1,1,0);
@@ -27,27 +32,6 @@ func davidReady():
 	cubeRow("++++++        ++++++",240,0,6,2,0,1,1,0);
 	cubeRow("++++++++++++++++++++",240,0,8,2,0,1,1,0);
 
-func nadiaReady():
-	cubeRow("++++++++++++++++++++",80,0,-10, 0 , 2, 4);
-	cubeRow("  +++++++++++++  +++",80,0,-8, 0 , 2, 4);
-	cubeRow("++++++++++++++   +++",80,0,-6, 0 , 2, 4);
-	cubeRow("++   ++++++++++    ++",80,0,-4, 0 , 2, 4);
-	cubeRow("+++  ++++++++++  ++++",80,0,-2, 0 , 2, 4);
-	cubeRow("++    ++++++   ++++++",80,0,0, 0 , 2, 4);
-	cubeRow("+++++  ++++++   +++++",80,0,2, 0 , 2, 4);
-	cubeRow("++++++  ++++++   ++++",80,0,4, 0 , 2, 4);
-	cubeRow("++    ++++++   ++++++",80,0,6, 0 , 2, 4);
-	cubeRow("++   ++++++++   +++++",80,0,8, 0 , 2, 4);
-	cubeRow("++   ++++++++   +++++",80,0,10, 0 , 2, 4);
-	cubeRow("+++  ++++++++   +++++",80,0,12, 0 , 2, 4);
-	cubeRow("    +++++++   +++++++",80,0,14, 0 , 2, 4);
-	cubeRow("+++++++++++++  ++++++",80,0,16, 0 , 2, 4);
-	cubeRow("++++++++++++   ++++++",80,0,18, 0 , 2, 4);
-	cubeRow("+++++++++++++   ++++++",80,0,20, 0 , 2, 4);
-	cubeRow("++++++++++++   ++++++++",80,0,22, 0 , 2, 4);
-	cubeRow("+++++++++++++  ++++++++",80,0,24, 0 , 2, 4);
-	cubeRow("+++++++++++++   ++++++++",80,0,26, 0 , 2, 4);
-	cubeRow("+++++++++++++  +++++++++",80,0,28, 0 , 2, 4);	
 	
 func jamesReady():
 	   cubeRow("++++++++++++++++    ++",200,0,-10,2,0,0,0,1);
@@ -61,45 +45,6 @@ func jamesReady():
 	   cubeRow("++++++    ++++++++++++",200,0,6,2,0,0,0,1);
 	   cubeRow("++++++    ++++++++++++",200,0,8,2,0,0,0,1);
 	   
-func agasthyaReady():
-	cubeMesh(0,-2,-8,39,39);
-	cubeRow("+++  + +++++++ +++++",0,0,-10);
-	cubeRow("+++ ++ ++++++    +++",0,0,-8);
-	cubeRow("+++    +++++++ +++++",0,0,-6);
-	cubeRow("+++ +++++++    +++++",0,0,-4);
-	cubeRow("+   +++++++ ++++++++",0,0,-2);
-	cubeRow("+ + +++++++ ++++++++",0,0,0);
-	cubeRow("+ + ++++++  ++++++++",0,0,2);
-	cubeRow("+ +  +++++ +++++++++",0,0,4);
-	cubeRow("+  +      ++++++++++",0,0,6);
-	cubeRow("++++ +++++++++++++++",0,0,8);
-
-func fatimaReady(): 
-	cubeRow("++++  ++ +++   ++ ++",40,0,8,2,0,1,1,0);
-	cubeRow(" +++ +++   ++++  +++",40,0,6,2,0,1,1,0);
-	cubeRow("++++++  +++++  ++++ ",40,0,4,2,0,1,0,1);
-	cubeRow("++++   +++ +++  ++++",40,0,2,2,0,0,1,0);
-	cubeRow("++ +++  ++++  ++  ++",40,0,0,2,0,1,1,0);
-	cubeRow("++  +++  ++  +++++ +",40,0,-2,2,0,0,1,0);
-	cubeRow("++   +  +++  +++  ++",40,0,-4,2,0,0,1,1);
-	cubeRow("++   ++ + +  +++  ++",40,0,-6,2,0,1,1,0);
-	cubeRow("   +++ +++    ++++++",40,0,-8,2,0,0,1,0);
-	cubeRow("+  ++   +++   ++  ++",40,0,-10,2,0,1,0,0);
-	cubeRow("+ ++++     ++ ++ +++",40,0,-12,2,0,1,1,0);
- 
-func yakariReady():
-	cubeMesh(0,-2,-10,30,30);
-	cubeRow("++++++++++++++++++ +",160,0,-10,2,0,1,1,0);
-	cubeRow("+++++    +++ +++++ +",160,0,-8,2,0,1,1,0);
-	cubeRow("++++++++ +++ +++++ +",160,0,-6,2,0,1,1,0);
-	cubeRow("++++++++     +++++ +",160,0,-4,2,0,1,1,0);
-	cubeRow("+++    +++++ +++++ +",160,0,-2,2,0,1,1,0);
-	cubeRow("++++++ +++++   ++  +",160,0,-0,2,0,1,1,0);
-	cubeRow("++ +++ +++  ++ +  ++",160,0,2,2,0,1,1,0);
-	cubeRow("++ +++     +++   +++",160,0,4,2,0,1,1,0);
-	cubeRow("++ +++++++ +++++++++",160,0,6,2,0,1,1,0);
-	cubeRow("++ +++++++ +++++++++",160,0,8,2,0,1,1,0);
-
 
 func _physics_process(_delta):
 	pass
@@ -114,13 +59,26 @@ func _physics_process(_delta):
 
 				
 
-func makeACube(x=0,y=0,z=0,r=1,g=0,b=0):
+func makeACube(x=0,y=0,z=0,r=1,g=0,b=0,xSize=2,ySize=2,zSize=2):
 	var sb = StaticBody.new();
-	var cs = CollisionShape.new();
-	cs.set_shape(BoxShape.new());
-	sb.add_child(cs);
+	
+	# this doesn't seem to work:
+	# var cs = CollisionShape.new();
+	# cs.set_shape(BoxShape.new());
+	# sb.add_child(cs);
+	
+	var boxShape = BoxShape.new();
+	printStr("boxShape:",boxShape);
+	var owner = sb.create_shape_owner(sb);
+	printStr("owner:",owner); 
+	sb.shape_owner_add_shape(owner, boxShape);
+	
+	#printStr("collision_layer_bit:",sb.get_collision_layer_bit(0));
+	#printStr("collision_mask_bit:",sb.get_collision_mask_bit(0));
+	
 	var m = MeshInstance.new();
 	var cm = CubeMesh.new();
+	cm.size = Vector3(xSize,ySize,zSize);
 	var sm = SpatialMaterial.new();
 	sm.albedo_color = Color( r, g, b, 1 );
 	cm.material = sm;
@@ -135,6 +93,7 @@ func makeASphere(x=0,y=0,z=0,r=1,g=0,b=0):
 	var sm = SphereMesh.new();
 	var mat = SpatialMaterial.new();
 	mat.albedo_color = Color(r,g,b,1);
+
 	sm.material = mat;
 	m.mesh = sm;
 	m.translation = Vector3(x,y,z);
@@ -160,3 +119,146 @@ func cubeMesh(x=0,y=0,z=0,xN=10,zN=10):
 			var calcZ = z + (n2*2);
 			# print("putting a cubemesh at " + str(calcX) + " " + str(y) + " " + str(calcZ));
 			makeACube(calcX,y,calcZ,0,1,0);
+
+func cubeVector(n=10,x=0,y=0,z=0,xInc=2,yInc=2,zInc=2,r=1,g=0,b=0):
+	for i in n:
+		makeACube(x,y,z,r,g,b);
+		x = x + xInc;
+		y = y + yInc;
+		z = z + zInc;
+
+func tree(x=0,y=0,z=0):
+	cubeVector(10,0+x,0+y,0+z,0,1,0,1,0,0);
+	makeACube(0+x,10+y,0+z,0,1,0,6,6,6);
+
+func printStr(a="",b="",c="",d=""):
+	print(str(a) + " " + str(b) + " " + str(c) + " " + str(d));
+
+func fatimaReady():
+	cubeMesh(0,-2,-10,30,30);
+	makeASphere(0,0,-10,0,0,1);
+	tree(0,0,0);
+	tree(10,0,0);
+	bush(12,-0.5,5)
+	tree1(15,0,0);
+	tree(20,2,0);
+	tree2(30,0,0);
+	bush(32,0,5)
+	tree1(35,0,8);
+	tree2(40,0,9);
+	tree(50,0,10);
+	bush(32,0,5)
+	tree1(34,0,-7);
+	tree2(37,0,5);
+	tree(30,0,-7);
+	bush(32,0.5,10)
+	tree(0,0,10);
+	tree1(10,0,10);
+	tree2(20,0,8);
+	bush(22,0,10)
+	tree(35,0,10);
+	tree1(30,0,18);
+	bush(37,1,10)
+	tree(32,0,20);
+	tree1(30,0,-17);
+	tree2(35,0,15);
+	tree1(25,0,-17);
+	
+func tree1(x=0,y=0,z=0):
+	cubeVector(10,0+x,0+y,0+z,0,1,0,1,0,0);
+	makeACube(0+x,8+y,0+z,0,100,0,8,8,8);
+	
+func tree2(x=0,y=0,z=0):
+	cubeVector(8,0+x,0+y,0+z,0,1,0,1,0,0);
+	makeACube(0+x,6+y,0+z,0,1,0,4,4,4);
+	
+func bush(x=0,y=0,z=0):
+	cubeVector(0,0+x,0+y,0+z,0,1,0,1,0,0);
+	makeACube(0+x,0+y,0+z,0,100,0,2,2,2);
+
+func agasthyaReady():
+	tTree(0,0,10);
+	tTree(10,0,20);
+	tTree(20,0,20);
+
+func tropicalTree(n=10,x=0,y=0,z=0,xInc=0,yInc=0.66,zInc=0,r=0.9,g=0.8,b=0.3):
+	for i in n:
+		makeASphere(x,y,z,r,g,b);
+		x = x + xInc;
+		y = y + yInc;
+		z = z + zInc;
+
+func tropicalLeaves(x=0,y=6.6,z=0,xInc=0.5,zInc=0.5):
+	cubeRow("+++++++++",x,y,z,xInc,zInc,0,1,0);
+	cubeRow("+++++++++",x,y,z,-xInc,zInc,0,1,0);
+	cubeRow("+++++++++",x,y,z,xInc,-zInc,0,1,0);
+	cubeRow("+++++++++",x,y,z,-xInc,-zInc,0,1,0);
+
+func tTree(x=0,y=0,z=0):
+	tropicalTree(10,0+x,0+y,0+z,0,0.66,0,0.9,0.8,0.3);
+	tropicalLeaves(0+x,6.6+y,0+z,0.5,0.5);
+
+
+
+func nadiaReady():
+	
+	house(30,0,25);
+	glitchyDecoration(15,0,30);
+	
+
+
+func house (x=0, y=0, z=0):
+	cubeVector(10,0+x,0+y,0+z,0,1,0,0,1,1); #corner
+	
+	# this block makes up a wall
+	cubeVector(10,1+x,0+y,0+z,0,1,0,0,1,1);
+	cubeVector(10,2+x,0+y,0+z,0,1,0,0,1,1);
+	cubeVector(10,3+x,0+y,0+z,0,1,0,0,1,1);
+	cubeVector(10,4+x,0+y,0+z,0,1,0,0,1,1);
+	cubeVector(10,5+x,0+y,0+z,0,1,0,0,1,1);
+	cubeVector(10,6+x,0+y,0+z,0,1,0,0,1,1);
+	cubeVector(10,7+x,0+y,0+z,0,1,0,0,1,1);
+	cubeVector(10,8+x,0+y,0+z,0,1,0,0,1,1);
+	
+	
+	
+	cubeVector(10,10+x,0+y,0+z,0,1,0,0,1,1); #corner
+	# making a wall
+	cubeVector(10,10+x,0+y,1+z,0,1,0,0,1,1);
+	cubeVector(10,10+x,0+y,2+z,0,1,0,0,1,1);
+	cubeVector(10,10+x,0+y,3+z,0,1,0,0,1,1);
+	cubeVector(10,10+x,0+y,4+z,0,1,0,0,1,1);
+	cubeVector(10,10+x,0+y,5+z,0,1,0,0,1,1);
+	cubeVector(10,10+x,0+y,6+z,0,1,0,0,1,1);
+	cubeVector(10,10+x,0+y,7+z,0,1,0,0,1,1);
+	cubeVector(10,10+x,0+y,8+z,0,1,0,0,1,1);
+	
+	
+	cubeVector(10,0+x,0+y,10+z,0,1,0,0,1,1); # corner
+	#making a wall
+	cubeVector(10,1+x,0+y,10+z,0,1,0,0,1,1);
+	cubeVector(10,2+x,0+y,10+z,0,1,0,0,1,1);
+	cubeVector(10,3+x,0+y,10+z,0,1,0,0,1,1);
+	cubeVector(10,4+x,0+y,10+z,0,1,0,0,1,1);
+	cubeVector(10,5+x,0+y,10+z,0,1,0,0,1,1);
+	cubeVector(10,6+x,0+y,10+z,0,1,0,0,1,1);
+	cubeVector(10,7+x,0+y,10+z,0,1,0,0,1,1);
+	cubeVector(10,8+x,0+y,10+z,0,1,0,0,1,1);
+	
+	
+	
+	cubeVector(10,10+x,0+y,10+z,0,1,0,0,1,1); # corner
+
+
+
+func glitchyDecoration(x=0,y=0,z=0):
+	cubeVector(10,0+x,0+y,1+z,0,1,0,1,0,1);
+	cubeVector(10,0+x,0+y,2+z,0,2,0,0,0,1);
+	cubeVector(10,0+x,0+y,3+z,0,3,0,1,0,1);
+	cubeVector(10,0+x,0+y,4+z,0,4,0,1,0,0);
+
+func cyrilReady():
+	makeASphere(0,9,3,0,4,6);
+	makeASphere(9,10,3,0,4,6);
+	cubeRow("++++  ++  ++++++++++ +",56,56,56,56,0,1,1,1);
+	cubeRow("++++  ++  ++++++++++ +",57,57,57,57,0,1,1,1);
